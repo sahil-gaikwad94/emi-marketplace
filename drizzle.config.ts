5a20f73a62eb2d797689194ff027679265fba59c
Sahil Gaikwad <166980003+sahil-gaikwad94@users.noreply.github.com>
2026-09-05 21:00:21 +0530

    Refactor database connection settings in config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,19 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is required to run drizzle commands");
 }
 
+const connectionUrl = new URL(connectionString);
+
 export default defineConfig({
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
   dialect: "mysql",
   dbCredentials: {
-    url: connectionString,
+    host: connectionUrl.hostname,
+    port: Number(connectionUrl.port || 4000),
+    user: decodeURIComponent(connectionUrl.username),
+    password: decodeURIComponent(connectionUrl.password),
+    database: connectionUrl.pathname.replace(/^\//, ""),
+    ssl: { rejectUnauthorized: true },
   },
 });
+
